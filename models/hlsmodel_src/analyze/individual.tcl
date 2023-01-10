d53acd7257b1f2c07bba20af622835b2de981c6f
H1MSK <ksda47832338@outlook.com>
2023-01-10 05:15:58 -0800

    (hls)Change: separate individual projects

--- models/hlsmodel_src/analyze/individual.tcl
+++ models/hlsmodel_src/analyze/individual.tcl
@@ -1,102 +1,118 @@
-open_project build
-exec mkdir -p ip
-
 # Linear<376, 64> forward
-open_solution -flow_target vivado sol_linear376_64_f
+open_project generated_linear_376_64_forward
+open_solution -flow_target vivado sol
 add_files -cflags "-DNodeType=Linear<376,64>" ../top_pg.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_forward_p
 
 csynth_design
+export_design -format ip_catalog -ipname linear_376_64_forward
 
 
 # Linear<376, 64> backward
-open_solution -flow_target vivado sol_linear376_64_b
+open_project generated_linear_376_64_backward
+open_solution -flow_target vivado sol
 add_files -cflags "-DNodeType=Linear<376,64>" ../top_pg.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_backward_p
 
 csynth_design
+export_design -format ip_catalog -ipname linear_376_64_backward
 
 
 # Linear<64, 64> forward
-open_solution -flow_target vivado sol_linear64_64_f
+open_project generated_linear_64_64_forward
+open_solution -flow_target vivado sol
 add_files -cflags "-DNodeType=Linear<64,64>" ../top_pg.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_forward_p
 
 csynth_design
+export_design -format ip_catalog -ipname linear_64_64_forward
 
 
 # Linear<64, 64> backward
-open_solution -flow_target vivado sol_linear64_64_b
+open_project generated_linear_64_64_backward
+open_solution -flow_target vivado sol
 add_files -cflags "-DNodeType=Linear<64,64>" ../top_pg.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_backward_p
 
 csynth_design
+export_design -format ip_catalog -ipname linear_64_64_backward
 
 
 # Linear<64, 17> forward
-open_solution -flow_target vivado sol_linear64_17_f
+open_project generated_linear_64_17_forward
+open_solution -flow_target vivado sol
 add_files -cflags "-DNodeType=Linear<64,17>" ../top_pg.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_forward_p
 
 csynth_design
+export_design -format ip_catalog -ipname linear_64_17_forward
 
 
 # Linear<64, 17> backward
-open_solution -flow_target vivado sol_linear64_17_b
+open_project generated_linear_64_17_backward
+open_solution -flow_target vivado sol
 add_files -cflags "-DNodeType=Linear<64,17>" ../top_pg.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_backward_p
 
 csynth_design
+export_design -format ip_catalog -ipname linear_64_17_backward
 
 
 # Tanh<64> forward
-open_solution -flow_target vivado sol_tanh64_f
+open_project generated_tanh_64_forward
+open_solution -flow_target vivado sol
 add_files -cflags "-DNodeType=Tanh<64>" ../top_g.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_forward
 
 csynth_design
+export_design -format ip_catalog -ipname tanh_64_forward
 
 
 # Tanh<64> backward
-open_solution -flow_target vivado sol_tanh64_b
+open_project generated_tanh_64_backward
+open_solution -flow_target vivado sol
 add_files -cflags "-DNodeType=Tanh<64>" ../top_g.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_backward
 
 csynth_design
+export_design -format ip_catalog -ipname tanh_64_backward
 
 
 # Exp<17> forward
-open_solution -flow_target vivado sol_exp17_f
+open_project generated_exp_17_forward
+open_solution -flow_target vivado sol
 add_files -cflags "-DNodeType=Exp<17>" ../top_g.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_forward
 
 csynth_design
+export_design -format ip_catalog -ipname exp_17_forward
 
 
 # Exp<17> backward
-open_solution -flow_target vivado sol_exp17_b
+open_project generated_exp_17_backward
+open_solution -flow_target vivado sol
 add_files -cflags "-DNodeType=Exp<17>" ../top_g.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_backward
 
 csynth_design
-cp build/sol_exp17_b/impl/export.zip ./ip/exp17_b.zip
+export_design -format ip_catalog -ipname exp_17_backward
