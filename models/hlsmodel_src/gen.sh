365f53dcefb1182118dcb250e0c3a52f7c4bbbe7
H1MSK <ksda47832338@outlook.com>
2023-01-17 06:30:37 -0800

    (hls) Add: more output in generate script

--- models/hlsmodel_src/gen.sh
+++ models/hlsmodel_src/gen.sh
@@ -3,6 +3,8 @@ python generator generated.nn.cpp generated.nn.tcl generated.fw_directives.tcl g
 
 echo -e "\033[32mGenerating IPs to be used in system...\033[0m"
 echo -e "\033[32mThis will take some time...\033[0m"
+echo -e "\033[32m(The output will seem to be messy since there are 4 jobs outputing to the\033[0m"
+echo -e "\033[32m console simultaneously. Please refer to build_*.log to check each output)\033[0m"
 sleep 1s
 vitis_hls generated.nn.tcl -l build_nn.log &
 vitis_hls generated.data_io.tcl -l build_data_io.log &
