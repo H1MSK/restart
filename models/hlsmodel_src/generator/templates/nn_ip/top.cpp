bf9712ca33bb794d20907f56012f186b0b985a3a
H1MSK <ksda47832338@outlook.com>
2023-01-18 04:22:55 -0800

    (hls ip) Change: Interface for control & cache_en

--- models/hlsmodel_src/generator/templates/nn_ip/top.cpp
+++ models/hlsmodel_src/generator/templates/nn_ip/top.cpp
@@ -1,10 +1,11 @@
 void nn_ip(hls::stream<cm_float>& in_x, hls::stream<cm_float, 35>& out_y, hls::stream<cm_float, 35>& in_grad_y, bool cache_en, $param_signatures, $grad_signatures) {
-    #pragma HLS INTERFACE mode=ap_ctrl_chain port=return
+    #pragma HLS INTERFACE mode=ap_ctrl_hs port=return
     #pragma HLS DATAFLOW
     #pragma HLS INTERFACE mode=axis port=in_x register_mode=reverse depth=376
     #pragma HLS INTERFACE mode=axis port=out_y register_mode=forward depth=35
     #pragma HLS INTERFACE mode=axis port=in_grad_y register_mode=reverse depth=35
-    #pragma HLS INTERFACE mode=s_axilite port=cache_en
+    #pragma HLS INTERFACE mode=ap_none port=cache_en
+    #pragma HLS STABLE variable=cache_en
 
 $param_hls_pragmas
 $grad_hls_pragmas
