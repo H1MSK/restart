f07b27ea95f26d543becc105f7a7651797fce747
H1MSK <ksda47832338@outlook.com>
2023-01-18 03:47:58 -0800

    (hls analyze) Change: Move top functions to subdir

--- models/hlsmodel_src/analyze/individual.tcl
+++ models/hlsmodel_src/analyze/individual.tcl
@@ -1,7 +1,7 @@
 # Linear<376, 64> forward
 open_project generated_linear_376_64_forward
 open_solution -flow_target vivado sol
-add_files -cflags "-DNodeType=Linear<376,64>" ../top_pg.cpp
+add_files -cflags "-DNodeType=Linear<376,64> -I.." top_pg.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_forward_p
@@ -13,7 +13,7 @@ export_design -format ip_catalog -ipname linear_376_64_forward
 # Linear<376, 64> backward
 open_project generated_linear_376_64_backward
 open_solution -flow_target vivado sol
-add_files -cflags "-DNodeType=Linear<376,64>" ../top_pg.cpp
+add_files -cflags "-DNodeType=Linear<376,64> -I.." top_pg.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_backward_p
@@ -25,7 +25,7 @@ export_design -format ip_catalog -ipname linear_376_64_backward
 # Linear<64, 64> forward
 open_project generated_linear_64_64_forward
 open_solution -flow_target vivado sol
-add_files -cflags "-DNodeType=Linear<64,64>" ../top_pg.cpp
+add_files -cflags "-DNodeType=Linear<64,64> -I.." top_pg.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_forward_p
@@ -37,7 +37,7 @@ export_design -format ip_catalog -ipname linear_64_64_forward
 # Linear<64, 64> backward
 open_project generated_linear_64_64_backward
 open_solution -flow_target vivado sol
-add_files -cflags "-DNodeType=Linear<64,64>" ../top_pg.cpp
+add_files -cflags "-DNodeType=Linear<64,64> -I.." top_pg.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_backward_p
@@ -49,7 +49,7 @@ export_design -format ip_catalog -ipname linear_64_64_backward
 # Linear<64, 17> forward
 open_project generated_linear_64_17_forward
 open_solution -flow_target vivado sol
-add_files -cflags "-DNodeType=Linear<64,17>" ../top_pg.cpp
+add_files -cflags "-DNodeType=Linear<64,17> -I.." top_pg.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_forward_p
@@ -61,7 +61,7 @@ export_design -format ip_catalog -ipname linear_64_17_forward
 # Linear<64, 17> backward
 open_project generated_linear_64_17_backward
 open_solution -flow_target vivado sol
-add_files -cflags "-DNodeType=Linear<64,17>" ../top_pg.cpp
+add_files -cflags "-DNodeType=Linear<64,17> -I.." top_pg.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_backward_p
@@ -73,7 +73,7 @@ export_design -format ip_catalog -ipname linear_64_17_backward
 # Tanh<64> forward
 open_project generated_tanh_64_forward
 open_solution -flow_target vivado sol
-add_files -cflags "-DNodeType=Tanh<64>" ../top_g.cpp
+add_files -cflags "-DNodeType=Tanh<64> -I.." top_g.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_forward
@@ -85,7 +85,7 @@ export_design -format ip_catalog -ipname tanh_64_forward
 # Tanh<64> backward
 open_project generated_tanh_64_backward
 open_solution -flow_target vivado sol
-add_files -cflags "-DNodeType=Tanh<64>" ../top_g.cpp
+add_files -cflags "-DNodeType=Tanh<64> -I.." top_g.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_backward
@@ -97,7 +97,7 @@ export_design -format ip_catalog -ipname tanh_64_backward
 # Exp<17> forward
 open_project generated_exp_17_forward
 open_solution -flow_target vivado sol
-add_files -cflags "-DNodeType=Exp<17>" ../top_g.cpp
+add_files -cflags "-DNodeType=Exp<17> -I.." top_g.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_forward
@@ -109,7 +109,7 @@ export_design -format ip_catalog -ipname exp_17_forward
 # Exp<17> backward
 open_project generated_exp_17_backward
 open_solution -flow_target vivado sol
-add_files -cflags "-DNodeType=Exp<17>" ../top_g.cpp
+add_files -cflags "-DNodeType=Exp<17> -I.." top_g.cpp
 create_clock -period 500MHz
 set_part xc7z020-clg400-2
 set_top top_backward
