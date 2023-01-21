8ff3691768c152642450f7a80caf0162778e877f
H1MSK <ksda47832338@outlook.com>
2023-01-21 00:54:06 -0800

    (hls script) Change: Message during generation

--- models/hlsmodel_src/gen.sh
+++ models/hlsmodel_src/gen.sh
@@ -3,12 +3,15 @@ python generator generated.nn.cpp generated.nn.tcl generated.fw_directives.tcl g
 
 echo -e "\033[32mGenerating IPs to be used in system...\033[0m"
 echo -e "\033[32mThis will take some time...\033[0m"
-echo -e "\033[32m(The output will seem to be messy since there are 4 jobs outputing to the\033[0m"
-echo -e "\033[32m console simultaneously. Please refer to build_*.log to check each output)\033[0m"
+echo -e "\033[32mThe output will seem to be messy since there are several jobs outputing to the console simultaneously.\033[0m"
+echo -e "\033[32mPlease refer to build_*.log to check each output.\033[0m"
 sleep 1s
 vitis_hls generated.nn.tcl -l build_nn.log &
 vitis_hls generated.data_io.tcl -l build_data_io.log &
 cd tool_ip && ./gen.sh && cd .. &
 wait
 
+echo -e "\033[32mGenerating system...\033[0m"
+echo -e "\033[32mThis will take some time...\033[0m"
 vivado -mode tcl -source generated.system.tcl
+echo -e "\033[32mFinished!\033[0m"
