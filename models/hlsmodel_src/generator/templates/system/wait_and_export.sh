8a4cfa89259225e1a5713c849f0d45e2203deb81
H1MSK <ksda47832338@outlook.com>
2023-02-06 02:55:22 -0800

    fix(hls gen): change export item xsa to bit&hwh

--- models/hlsmodel_src/generator/templates/system/wait_and_export.sh
+++ models/hlsmodel_src/generator/templates/system/wait_and_export.sh
@@ -3,11 +3,12 @@ open_project build_system/system.xpr
 
 wait_on_runs impl_1
 
-write_hw_platform -fixed -include_bit -force -file ./generated.system.$struct_id.xsa
+file copy -force ./build_system/system.gen/sources_1/bd/system/hw_handoff/system.hwh ./generated.system.$struct_id.hwh
+file copy -force ./build_system/system.runs/impl_1/system_wrapper.bit ./generated.system.$struct_id.bit
 
 exit
 EOF
 
-vivado -mode tcl -source generated.wait_runs_and_export_system.tcl -nojournal -log wait_and_export_system.log
+vivado -mode tcl -source generated.wait_runs_and_export_system.tcl -nojournal -log generated.wait_and_export_system.log
 
 rm generated.wait_runs_and_export_system.tcl
