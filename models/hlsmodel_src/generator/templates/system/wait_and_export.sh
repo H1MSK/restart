f57f63d8fb37ccd3ef055c793eda3b78d59a1ac7
H1MSK <ksda47832338@outlook.com>
2023-02-06 21:34:19 -0800

    perf(hlsGen): improve robustness

--- models/hlsmodel_src/generator/templates/system/wait_and_export.sh
+++ models/hlsmodel_src/generator/templates/system/wait_and_export.sh
@@ -3,6 +3,10 @@ open_project build_system/system.xpr
 
 wait_on_runs impl_1
 
+if {[get_property PROGRESS [get_runs impl_1]] != "100%"} {
+   error "ERROR: impl failed, stop exporting"
+}
+
 file copy -force ./build_system/system.gen/sources_1/bd/system/hw_handoff/system.hwh ./generated.system.$struct_id.hwh
 file copy -force ./build_system/system.runs/impl_1/system_wrapper.bit ./generated.system.$struct_id.bit
 
