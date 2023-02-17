70e169a2753d70b8949e6c73100fb2a598f4a43a
H1MSK <ksda47832338@outlook.com>
2023-02-17 16:34:26 +0000

    wip(hls): driver for s_axilite ap control

--- models/hlsmodel.py
+++ models/hlsmodel.py
@@ -2,12 +2,13 @@ from itertools import chain
 import os
 from models.interfaces import AbstractActorCritic
 import torch
+import numpy as np
 from typing import Callable, Tuple
 import pynq
 from pynq import GPIO
 import time
 import logging
-from cm_type import get_float_pointer, torch_cm_float
+from cm_type import get_float_pointer, torch_cm_float, np_cm_float
 
 from models.pymodel import PyModel
 
@@ -28,39 +29,6 @@ class _FuncFlipFlop:
             self.func()
         assert(self.diff > -2)
 
-class ApController:
-    def __init__(self, name: str, control_start_pin: int, state_monitor_pin: int):
-        self.name = name
-        self.start_o    = GPIO(GPIO.get_gpio_pin(control_start_pin+0x00), 'out')
-        self.compl_o    = GPIO(GPIO.get_gpio_pin(control_start_pin+0x01), 'out')
-        self.done_i     = GPIO(GPIO.get_gpio_pin(control_start_pin+0x02), 'in')
-        self.idle_i     = GPIO(GPIO.get_gpio_pin(control_start_pin+0x03), 'in')
-        self.ap_start   = GPIO(GPIO.get_gpio_pin(state_monitor_pin+0x00), 'in')
-        self.ap_done    = GPIO(GPIO.get_gpio_pin(state_monitor_pin+0x01), 'in')
-        self.ap_ready   = GPIO(GPIO.get_gpio_pin(state_monitor_pin+0x02), 'in')
-        self.ap_idle    = GPIO(GPIO.get_gpio_pin(state_monitor_pin+0x03), 'in')
-    
-    def state_str(self):
-        return (f"State:{self.done_i.read()}{self.idle_i.read()}{self.ap_start.read()}"
-                f"{self.ap_done.read()}{self.ap_ready.read()}{self.ap_idle.read()}")
-
-    def wait_to_complete(self):
-        self.compl_o.write(1)
-        cnt=0
-        while not self.idle_i.read() or self.done_i.read():
-            cnt+=1
-            print(f"{cnt}:wait {self.name} to complete({self.state_str()})...",
-                  end='\r')
-        self.compl_o.write(0)
-    
-    def start(self):
-        self.start_o.write(1)
-        cnt = 0
-        while self.idle_i.read() and not self.done_i.read():
-            cnt += 1
-            print(f"{cnt}: wait {self.name} to start({self.state_str})...", end='\r')
-        self.start_o.write(0)
-
 class HlsActorCritic(AbstractActorCritic):
     def __init__(self, obs_dim, act_dim, /, lr_actor=0.0001, lr_critic=0.001, hidden_width=64, act_continuous=True, use_orthogonal_init=False) -> None:
         super().__init__(obs_dim, act_dim, lr_actor, lr_critic, hidden_width, act_continuous, use_orthogonal_init)
@@ -70,14 +38,9 @@ class HlsActorCritic(AbstractActorCritic):
 
         self._init_net_parameters(lr_critic, lr_actor, use_orthogonal_init)
 
-        self._init_holders()
-
         self.backward_fff = _FuncFlipFlop(self._actual_backward)
         self.step_fff = _FuncFlipFlop(self._actual_step)
         self.zero_grad_fff = _FuncFlipFlop(self._actual_zero_grad)
-
-    def _init_holders(self):
-        self.grads = torch.zeros_like(self.params)
 
     def _init_backend(self):
         bitfile_path = os.path.join(
@@ -108,11 +71,6 @@ class HlsActorCritic(AbstractActorCritic):
 
         self.bram_sel_o    = GPIO(GPIO.get_gpio_pin(0x19), 'out')
 
-        self.fw_ap = ApController("fw", 0x00, 0x20)
-        self.bw_ap = ApController("bw", 0x04, 0x24)
-        self.pa_ap = ApController("pa", 0x08, 0x28)
-        self.gr_ap = ApController("gr", 0x0c, 0x2c)
-        
         _logger.info("Resetting PL IPs...")
         self.sys_reset_o.write(1)
         time.sleep(0.001)
@@ -145,16 +103,18 @@ class HlsActorCritic(AbstractActorCritic):
 
     def _set_net_parameters(self, /, lr_critic: float, lr_actor: float, params: torch.Tensor):
         self.lr_critic_modifier = lr_critic / lr_actor
+        self.lr_actor = lr_actor
 
-        if params.dtype != torch_cm_float:
-            params = torch.tensor(params, dtype=torch_cm_float)
-        self.params = params.detach().requires_grad_(True)
+        self.params = pynq.allocate(params.shape, dtype=np_cm_float)
+        np.copyto(self.params, params.detach().numpy(), casting='same_kind')
+        self.grads = pynq.allocate(params.shape, dtype=np_cm_float)
+        self.params_tensor = torch.from_numpy(self.params).requires_grad_(True)
 
         self._apply_params()
 
         assert(self.act_continuous)
         self.optim = torch.optim.Adam(
-            [self.params],
+            [self.params_tensor],
             lr=lr_actor,
             eps=1e-5
         )
@@ -163,15 +123,17 @@ class HlsActorCritic(AbstractActorCritic):
         _logger.info("Parameters syncing out...")
         self.bram_sel_o.write(0)
 
-        self.pa_ap.wait_to_complete()
+        self.params.sync_to_device()
 
-        self.ip_param_loader.mmio.write(
-            self.ip_param_loader.register_map.in_r.address,
-            get_float_pointer(self.params.detach())
-        )
+        while not self.ip_param_loader.register_map.CTRL.AP_IDLE:
+            pass
 
-        self.pa_ap.start()
-        self.pa_ap.wait_to_complete()
+        self.ip_param_loader.register_map.in_r=self.params.device_address
+
+        self.ip_param_loader.register_map.CTRL.AP_START=1
+
+        while not self.ip_param_loader.register_map.CTRL.AP_DONE:
+            pass
 
         _logger.info("Parameters syncing finished!")
 
@@ -183,40 +145,36 @@ class HlsActorCritic(AbstractActorCritic):
     def lr_critic(self, value):
         self.lr_critic_modifier = value / self.lr_actor
 
-    @property
-    def lr_actor(self):
-        return self.optim.param_groups[0]['lr']
-    
-    @lr_actor.setter
-    def lr_actor(self, value):
-        for p in self.optim.param_groups:
-            p['lr'] = value
-
     def forward(self, obs: torch.Tensor, requires_grad=False) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        holder = torch.zeros((len(obs), self.act_dim * 2 + 1), dtype=torch_cm_float)
-        mu = holder[:, 0:self.act_dim]
-        sigma = holder[:, self.act_dim:self.act_dim*2]
-        value = holder[:, self.act_dim*2:]
+        mu = np.zeros((len(obs), self.act_dim), dtype=np_cm_float)
+        sigma = np.zeros((len(obs), self.act_dim), dtype=np_cm_float)
+        value = np.zeros((len(obs), 1), dtype=np_cm_float)
 
+        holder_x = pynq.allocate((self.obs_dim, ), dtype=np_cm_float)
+        holder_y = pynq.allocate((self.act_dim * 5 + 1, ), dtype=np_cm_float)
         self.bram_sel_o.write(1)
         self.cache_en_o.write(1 if requires_grad else 0)
 
+        while not self.ip_forward.register_map.CTRL.AP_IDLE:
+            pass
+
         for i, o in enumerate(obs):
-            self.fw_ap.wait_to_complete()
             print(f"Forwarding #{i:2d}...", end="\r")
-            self.ip_forward.mmio.write(
-                self.ip_forward.register_map.maxi_x.address,
-                get_float_pointer(o))
-            self.ip_forward.mmio.write(
-                self.ip_forward.register_map.maxi_y.address,
-                get_float_pointer(holder[i]))
-            self.fw_ap.start()
+            np.copyto(holder_x, o.numpy())
+            holder_x.sync_to_device()
+            self.ip_forward.register_map.maxi_x=holder_x.device_address
+            self.ip_forward.register_map.maxi_y=holder_y.device_address
+            self.ip_forward.register_map.CTRL.AP_START=1
+            while not self.ip_forward.register_map.CTRL.AP_DONE:
+                pass
+            holder_y.sync_from_device()
+            mu[i, :]=holder_y[:self.act_dim]
+            sigma[i, :]=holder_y[self.act_dim:self.act_dim*2]
+            value[i, 0]=holder_y[self.act_dim*2]
 
-        self.fw_ap.wait_to_complete()
-
-        return (value.detach().requires_grad_(requires_grad),
-                mu.detach().requires_grad_(requires_grad),
-                sigma.detach().requires_grad_(requires_grad))
+        return (torch.from_numpy(value).requires_grad_(requires_grad),
+                torch.from_numpy(mu).requires_grad_(requires_grad),
+                torch.from_numpy(sigma).requires_grad_(requires_grad))
 
     def act(self, obs: torch.Tensor, requires_grad=False) -> Tuple[torch.Tensor, torch.Tensor]:
         # To avoid messing up cache
@@ -241,40 +199,48 @@ class HlsActorCritic(AbstractActorCritic):
         assert(len(grads.shape) == 2)
         self.bram_sel_o.write(1)
 
+        holder = pynq.allocate(grads.shape[1:], dtype=np_cm_float)
+
+        while not self.ip_backward.register_map.CTRL.AP_IDLE:
+            pass
 
         for i, g in enumerate(grads):
             print(f"Backwarding #{i:2d}...", end="\r")
-            self.bw_ap.wait_to_complete()
-            self.ip_backward.mmio.write(
-                self.ip_backward.register_map.maxi_grad_y.address,
-                get_float_pointer(g)
-            )
-            self.bw_ap.start()
+            
+            np.copyto(holder, g.numpy())
+            holder.sync_to_device()
+            self.ip_backward.register_map.maxi_grad_y=holder.device_address
+            self.ip_backward.register_map.CTRL.AP_START=1
+            while not self.ip_backward.register_map.CTRL.AP_DONE:
+                pass
 
-        self.bw_ap.wait_to_complete()
         
     def _actual_zero_grad(self):
         self.gr_reset_o.write(1)
-        time.sleep(0)
+        # TODO: test this
+        while not self.gr_rst_busy_i.read():
+            pass
         self.gr_reset_o.write(0)
         while self.gr_rst_busy_i.read():
-            time.sleep(0)
+            pass
 
     def _extract_grads(self):
         self.bram_sel_o.write(0)
 
-        self.gr_ap.wait_to_complete()
-        
-        self.ip_grad_extractor.mmio.write(
-            self.ip_grad_extractor.register_map.out_r.address,
-            get_float_pointer(self.grads)
-        )
-        self.gr_ap.start()
-        self.gr_ap.wait_to_complete()
-        self.params.grad = self.grads
+        while not self.ip_grad_extractor.register_map.CTRL.AP_IDLE:
+            pass
 
+        self.ip_grad_extractor.register_map.out_r=self.grads.device_address
+
+        self.ip_grad_extractor.register_map.CTRL.AP_START=1
+        while not self.ip_grad_extractor.register_map.CTRL.AP_DONE:
+            pass
+
+        self.grads.sync_from_device()
+    
     def _actual_step(self):
         self._extract_grads()
+        self.params_tensor.grad = torch.from_numpy(self.grads)
         self.optim.step()
         self._apply_params()
 
