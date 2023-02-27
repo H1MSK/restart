cf05e058f22ba846d6ae7a3c6339e19548e27b34
H1MSK <ksda47832338@outlook.com>
2023-02-27 05:35:33 -0800

    fix(HlsIp): several errors of sync_fifo_controller

--- models/hlsmodel_src/tool_ip/sync_fifo_controller.v
+++ models/hlsmodel_src/tool_ip/sync_fifo_controller.v
@@ -69,13 +69,13 @@ module sync_fifo_controller#(
     reg[ADDR_WIDTH-1:0]  read_addr;
     reg[ADDR_WIDTH-1:0]  write_addr;
 
-    reg[ADDR_WIDTH:0]  data_cnt;
+    reg[ADDR_WIDTH:0]  data_cnt = 0;
 
     reg fifo_empty;
     reg fifo_full;
 
-    wire next_fifo_full = (data_cnt == SIZE);
-    wire next_fifo_empty = (data_cnt == 0);
+    wire next_fifo_full = (data_cnt == SIZE && ~fifo_rd_en || data_cnt == SIZE-1 && fifo_wr_en);
+    wire next_fifo_empty = (data_cnt == 0 && ~fifo_wr_en || data_cnt == 1 && fifo_rd_en);
 
     assign fifo_empty_n = !fifo_empty;
     assign fifo_full_n = !fifo_full;
@@ -119,20 +119,20 @@ module sync_fifo_controller#(
     always@(posedge clk)
     if (internal_reset)
         read_addr <= 0;
-    else if (fifo_rd_en && ~next_fifo_empty)
+    else if (fifo_rd_en && ~fifo_empty)
         read_addr <= next_read_addr;
 
     // write data address
     always@(posedge clk)
     if (internal_reset)
         write_addr <= 0;
-    else if (fifo_wr_en && ~next_fifo_full)
-        write_addr <= next_read_addr;
+    else if (fifo_wr_en && ~fifo_full)
+        write_addr <= next_write_addr;
 
     // full register
     always@(posedge clk)
     if (internal_reset)
-        fifo_full <= 0;
+        fifo_full <= 1;
     else
         fifo_full <= next_fifo_full;
 
@@ -142,16 +142,20 @@ module sync_fifo_controller#(
         fifo_empty <= 1;
     else
         fifo_empty <= next_fifo_empty;
+    
+    reg[ADDR_WIDTH:0]  next_data_cnt;
+    
+    always@(*) begin
+        if (~fifo_full && fifo_wr_en && ~fifo_rd_en) next_data_cnt <= data_cnt + 1;
+        else if (~fifo_empty && fifo_rd_en && ~fifo_wr_en) next_data_cnt <= data_cnt - 1;
+        else next_data_cnt <= data_cnt;
+    end
 
     // fifo data count
     always@(posedge clk)
     if (internal_reset)
         data_cnt <= 0;
-    else if (fifo_wr_en && ~fifo_rd_en && ~next_fifo_full)
-        data_cnt <= data_cnt + 1;
-    else if (fifo_rd_en && ~fifo_wr_en && ~next_fifo_empty)
-        data_cnt <= data_cnt - 1;
     else
-        data_cnt <= data_cnt;
+        data_cnt <= next_data_cnt;
 
 endmodule
